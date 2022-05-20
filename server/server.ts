cda41f9607bf4ae1205744c032a36a4a885b4b34
sara <sara.wikdahl@iths.se>
2022-05-20 13:51:13 +0200

    add dotenv

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,13 @@ import express, { Request, Response, json} from 'express';
 import recipeRouter from './Routes/recipes';
 import categoryRouter from './Routes/category';
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import { connect } from 'mongoose'
 connect('mongodb+srv://SaraWikdahl:ZBBGFr7EBmuQ3cxC@cluster0.2sq41.mongodb.net/Recept?retryWrites=true&w=majority')
 // connect('mongodb://localhost:27017/Recept')
 
+dotenv.config();
 
 const app = express();
 
@@ -15,6 +17,14 @@ app.use(cors())
 app.use(json());
 // const port = 4000
 const port = process.env.PORT || 4000
+
+if (process.env.MONGO_DB_CONNECTION_STRING) {
+    connect(process.env.MONGO_DB_CONNECTION_STRING).then(() => {
+        app.listen(port, () => console.log('listening to port: ' + port));
+    })
+} else {
+    console.log('Configuration MONGO_DB_CONNECTION_STRING not set')
+}
 
 //routes
 app.use('/recipes', recipeRouter);
